725ea282815ebc648b2a54206608e3814d198460
Ashish Kaintura <ashu88793@gmail.com>
2025-08-04 10:54:16 +0530

    spell check

--- src/components/WhyJoinSectionLight.jsx
+++ src/components/WhyJoinSectionLight.jsx
@@ -61,7 +61,7 @@ const WhyJoinSectionLight = () => {
             data-aos-delay="100"
             className="mt-4 text-lg text-gray-700 max-w-3xl mx-auto"
           >
-            At <span className="text-pink-600 font-bold">AI fRisezonic</span>, we
+            At <span className="text-pink-600 font-bold">AI Risezonic</span>, we
             don’t believe in boring lectures. We believe in hands-on learning,
             real-world projects, and future-ready skills.
           </p>
